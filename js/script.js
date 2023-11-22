bfb4523da77a2bb3cf096ee1a33b8d1cb63c8dc9
benjaAguilar <benjaaguilar02@gmail.com>
2023-11-22 15:45:32 -0300

    Created a JSON deckOfCards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+let deckOfCards = {
+   "diamond": ["as", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "j", "q", "k"],
+   "hearth": ["as", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "j", "q", "k"],
+   "pikas": ["as", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "j", "q", "k"],
+   "clover": ["as", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "j", "q", "k"],
+}
 let getCard = document.querySelector("#get-card");
 
 getCard.addEventListener("click", function(){
